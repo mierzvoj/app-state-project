a48ebf766ac367ff0b99131c7479398de2990eb7
Slawomir Kussowski <skussowski@advaoptical.com>
2022-05-13 21:40:48 +0200

    added pokemons views

--- src/pages/PokemonsToolbar/PokemonsToolbar.tsx
+++ src/pages/PokemonsToolbar/PokemonsToolbar.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import BackDefault from "./PokemonSprites/BackDefault/BackDefault";
+import FrontDefault from "./PokemonSprites/FrontDefault/FrontDefault";
 
 export interface LocationState {
   url: string;
@@ -8,11 +11,29 @@ export interface LocationState {
 function PokemonsToolbar() {
   const { state } = useLocation();
   const { url, name } = state as LocationState;
+
+  const [appState, setAppState] = useState<any>({});
+
+  useEffect(() => {
+    if (url) {
+      fetch(url)
+        .then(
+          (response) => response.json(),
+          (error) => console.error(error)
+        )
+        .then((data) => {
+          setAppState({ data });
+        });
+    }
+  }, [url]);
+
   return (
     <>
       <span>{url}</span>
       <span>{name}</span>
       <Link to="/">Home</Link>
+      <FrontDefault url={appState?.data?.sprites?.front_default}></FrontDefault>
+      <BackDefault url={appState?.data?.sprites?.back_default}></BackDefault>
     </>
   );
 }
