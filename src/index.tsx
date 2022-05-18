f93dde7ec89dc0367f61a3d4546aa0b7fef1e236
Slawomir Kussowski <skussowski@advaoptical.com>
2022-05-18 21:51:00 +0200

    userform introduced

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login/Login";
 import NotFound from "./pages/NotFound/NotFound";
 import Pokemons from "./pages/Pokemons/Pokemons";
 import PokemonsToolbar from "./pages/PokemonsToolbar/PokemonsToolbar";
+import UserForm from "./pages/UserForm/UserForm";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -16,6 +17,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}></Route>
+        <Route path="/userform" element={<UserForm />}></Route>
         <Route path="pokemons" element={<Pokemons />}>
           <Route path=":name" element={<PokemonsToolbar />}></Route>
         </Route>
--- src/pages/UserForm/UserForm.tsx
+++ src/pages/UserForm/UserForm.tsx
@@ -1,23 +1,37 @@
 import {
   Button,
+  Checkbox,
   FormControl,
+  FormControlLabel,
   FormGroup,
   FormLabel,
   IconButton,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
   Snackbar,
   TextField,
 } from "@material-ui/core";
 import CloseIcon from "@mui/icons-material/Close";
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import "./UserForm.css";
 
 export interface LoginData {
+  name?: string;
+  surname?: string;
+  city?: string;
   email?: string;
   password?: string;
 }
 
-export default function Login() {
+const cities: string[] = ["Gdańsk", "Gdynia", "Sopot", "Warszawa", "Zakopane"];
+
+export default function UserForm() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [city, setCity] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [open, setOpen] = useState(false);
@@ -30,7 +44,7 @@ export default function Login() {
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (email === "test@test" && password === "test") {
-      navigate("/userform");
+      navigate("/pokemons");
     } else {
       setOpen(true);
     }
@@ -63,10 +77,54 @@ export default function Login() {
     <div className="Login">
       <form onSubmit={(e: FormEvent<HTMLFormElement>) => handleSubmit(e)}>
         <FormGroup>
-          <FormLabel>Email</FormLabel>
+          <FormLabel>Name</FormLabel>
           <FormControl>
             <TextField
               autoFocus
+              value={name}
+              onChange={(
+                e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+              ) => setName(e.target.value)}
+            />
+          </FormControl>
+          <FormLabel>Surname</FormLabel>
+          <FormControl>
+            <TextField
+              value={surname}
+              onChange={(
+                e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+              ) => setSurname(e.target.value)}
+            />
+          </FormControl>
+          <FormLabel>City</FormLabel>
+          <FormControl>
+            <Select
+              value={city}
+              onChange={(e) => setCity(e.target.value as string)}
+            >
+              {cities.map((c) => (
+                <MenuItem value={c}>{c}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+            <RadioGroup defaultValue="female">
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+            </RadioGroup>
+          </FormControl>
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="Active"
+          />
+          <FormLabel>Email</FormLabel>
+          <FormControl>
+            <TextField
               type="email"
               value={email}
               onChange={(
@@ -85,9 +143,10 @@ export default function Login() {
             />
           </FormControl>
         </FormGroup>
-        <Button type="submit" color="primary" disabled={!validateForm()}>
-          Login
+        <Button type="submit" color="default" disabled={!validateForm()}>
+          Apply
         </Button>
+        <Button color="secondary">Reset</Button>
       </form>
       <Snackbar
         open={open}
