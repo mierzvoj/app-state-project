f48d9cc2e22e9f6ce776ba76337ccc91748a5863
mierzvoj <mierzvoj@gmail.com>
2022-06-03 19:43:58 +0200

    testing

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,9 @@ import PokemonsToolbar from "./pages/Pokemons/PokemonsToolbar/PokemonsToolbar";
 import UserForm from "./pages/Users/UserForm/UserForm";
 import Users from "./pages/Users/Users";
 import UsersList from "./pages/Users/UsersList/UsersList";
-
+// git conflict test
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement // git conflict test
 );
 root.render(
   <React.StrictMode>
